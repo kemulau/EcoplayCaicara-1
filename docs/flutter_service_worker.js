cd6501b0385613acc7da4a7c01e1a574389d986a
Kemuly Lau <kemuly8@gmail.com>
2025-09-20 12:59:56 -0300

    test safari

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -74,7 +74,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0ec466ceab405a87dce8947a09692bb3
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "556f891def0e2adbc33b3e9a0e81855f",
+"flutter_bootstrap.js": "272eef08e09e3a9c0921f7b2c0c6dc47",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
